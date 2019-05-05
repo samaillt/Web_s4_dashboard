a70f650475f308d0899e0f5529a305c7a74cad6a
torresf <torres.florian1@gmail.com>
2019-05-05 12:23:21 +0200

    Style fixed

--- src/components/TopGames.js
+++ src/components/TopGames.js
@@ -3,7 +3,7 @@ import { h } from 'hyperapp'
 export default (props) => (
 	<ul class="top-games d-flex flex-wrap justify-content-between py-3">
 		{props.topGames && props.topGames.map(g => (
-			<li key={g.id} class="my-3 mx-1">
+			<li key={g.id} class="my-3 mx-1 d-flex flex-column">
 				<img src={g.game.box.template.replace('{width}', '285').replace('{height}', '380')} alt={g.name} />
 				<div class="legend">
 					<p class="name">{g.game.name}</p>
