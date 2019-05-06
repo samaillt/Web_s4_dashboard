3019a9bb66f00ea873cc80e5caeb8e933abc24ff
Tom Samaille <tomsamaille@gmailcom>
2019-05-06 09:14:55 +0200

    Styling profile

--- src/components/ProfileInfos.js
+++ src/components/ProfileInfos.js
@@ -1,7 +1,7 @@
 import { h } from 'hyperapp'
 
 export default (props) => (
-	<div>
+	<div id="profile-container">
 		<div class="profile-infos">
 			<p className='value'>{props.profile.followers}</p>
 			<p className='name'>Followers</p>
@@ -10,6 +10,9 @@ export default (props) => (
 			<p className='value'>{props.profile.views}</p>
 			<p className='name'>Views</p>
 		</div>
-		<img class="profile" src={props.profile.logo} alt={props.profile.display_name}/>
+		<div class="profile-card">
+			<img class="profile" src={props.profile.logo} alt={props.profile.display_name}/>
+			<p className='name'>{props.profile.display_name}</p>
+		</div>
 	</div>
 );
